7c2ea3b933c3e085c8302d94111f895e55287462
Denis Lapin <kviakit@Gmail.com>
2023-09-01 22:12:53 +0300

    Replaced http-url-connection with spring-web-client.

--- src/main/java/ru/kviak/geocodingapi/dto/GeoCodeResponseDto.java
+++ src/main/java/ru/kviak/geocodingapi/dto/GeoCodeResponseDto.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ru.kviak.geocodingapi.dto.map_dto.PositionDto;
+import ru.kviak.geocodingapi.dto.mapdto.PositionDto;
 
 @Setter
 @Getter
